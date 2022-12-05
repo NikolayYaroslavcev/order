d410ac1ba97a1f517c54350ae6965e911b9d5e2b
Николай <80519843+NikolayYaroslavcev@users.noreply.github.com>
2022-12-05 11:16:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    document.querySelector('.video__iframe').src = 'https://www.youtube.com/embed/jKNdGbyrzZg';
+  }, 3000);
+});
